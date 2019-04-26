31641298e1202fdba54cb8b5484239e8bdfcaabd
Victor Milovanov <lost@losttech.software>
2019-04-26 13:22:08 -0700

    added a PowerShell script to download the original GPT-2 model

---
+++ GPT-2/download_model.ps1
@@ -0,0 +1,12 @@
+﻿param([String]$name="117M")
+
+mkdir -Force models/$name
+
+foreach ($filename in @("checkpoint","encoder.json","hparams.json",
+			"model.ckpt.data-00000-of-00001",
+			"model.ckpt.index","model.ckpt.meta","vocab.bpe"))
+{
+	$fetch = "$name/$filename"
+	echo "fetching $fetch"
+	Invoke-WebRequest -OutFile "models/$fetch" -Uri "https://storage.googleapis.com/gpt-2/models/$fetch"
+}
