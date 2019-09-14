721b73f8df015e44b8eaf562a24c7626e485fc6e
Victor Milovanov <lost@losttech.software>
2019-09-14 11:50:29 -0700

    split the ResNetBlock Main body to allow invocation from CI

--- ResNetBlock/ResNetSampleProgram.cs
+++ ResNetBlock/ResNetSampleProgram.cs
@@ -2,17 +2,19 @@
     using System;
     using tensorflow;
     static class ResNetSampleProgram {
-        static void Main() {
-            GradientLog.OutputWriter = Console.Out;
-            GradientSetup.UseEnvironmentFromVariable();
-
-            var block = new ResNetBlock(kernelSize: 1, filters: new[] { 1,2,3 });
+        public static void Run() {
+            var block = new ResNetBlock(kernelSize: 1, filters: new[] { 1, 2, 3 });
             var applicationResult = block.call(tf.zeros(new TensorShape(1, 2, 3, 3)));
             Console.WriteLine(applicationResult);
 
-            foreach(dynamic variable in block.trainable_variables) {
+            foreach (dynamic variable in block.trainable_variables) {
                 Console.WriteLine(variable.name);
             }
+        }
+        static void Main() {
+            GradientLog.OutputWriter = Console.Out;
+            GradientSetup.UseEnvironmentFromVariable();
+            Run();
         }
     }
 }
