73ab817c4791cb745f47ceaae33d3c05b794b80c
Victor Milovanov <lost@losttech.software>
2019-09-14 11:37:59 -0700

    an implementation (broken) of ResNetBlock sample

---
+++ ResNetBlock/ResNetBlock.cs
@@ -0,0 +1,33 @@
+﻿namespace Gradient.Samples {
+    using tensorflow;
+    using tensorflow.keras;
+    using tensorflow.keras.layers;
+
+    class ResNetBlock: Model {
+        const int PartCount = 3;
+        readonly Conv2D[] convs = new Conv2D[PartCount];
+        readonly BatchNormalization[] batchNorms = new BatchNormalization[PartCount];
+        public ResNetBlock(int kernelSize, int[] filters) {
+            for (int part = 0; part < this.convs.Length; part++) {
+                this.convs[part] = part == 1
+                    ? Conv2D.NewDyn(filters[part], kernel_size: kernelSize, padding: "same")
+                    : Conv2D.NewDyn(filters[part], kernel_size: (1, 1));
+            }
+        }
+
+        public override object call(object inputs, bool training, object mask = null) {
+            dynamic result = inputs;
+
+            for(int part = 0; part < this.convs.Length; part++) {
+                result = this.convs[part].call(result);
+                result = this.batchNorms[part].call(result);
+                if (part + 1 != this.convs.Length)
+                    result = tf.nn.relu(result);
+            }
+
+            result += inputs;
+
+            return tf.nn.relu(result);
+        }
+    }
+}
---
+++ ResNetBlock/ResNetSampleProgram.cs
@@ -0,0 +1,18 @@
+﻿namespace Gradient.Samples {
+    using System;
+    using tensorflow;
+    static class ResNetSampleProgram {
+        static void Main() {
+            GradientLog.OutputWriter = Console.Out;
+            GradientSetup.UseEnvironmentFromVariable();
+
+            var block = new ResNetBlock(kernelSize: 1, filters: new[] { 1,2,3 });
+            var applicationResult = block.call(tf.zeros(new TensorShape(1, 2, 3, 3)));
+            Console.WriteLine(applicationResult);
+
+            foreach(dynamic variable in block.trainable_variables) {
+                Console.WriteLine(variable.name);
+            }
+        }
+    }
+}
