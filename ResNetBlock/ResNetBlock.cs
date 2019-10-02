03aa035080d3a46fe6a4c8dcfd6e8f1b91a414a7
Victor <lost@losttech.software>
2019-10-01 18:49:59 -0700

    Added a model subclassing sample: ResNetBlock (#2)
    also: use Gradient preview 6.2

---
+++ ResNetBlock/ResNetBlock.cs
@@ -0,0 +1,54 @@
+﻿namespace Gradient.Samples {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Gradient.ManualWrappers;
+    using SharPy.Runtime;
+    using tensorflow;
+    using tensorflow.keras;
+    using tensorflow.keras.layers;
+
+    class ResNetBlock: Model {
+        const int PartCount = 3;
+        readonly PythonList<Conv2D> convs = new PythonList<Conv2D>();
+        readonly PythonList<BatchNormalization> batchNorms = new PythonList<BatchNormalization>();
+        public ResNetBlock(int kernelSize, int[] filters) {
+            for (int part = 0; part < PartCount; part++) {
+                this.convs.Add(this.Track(part == 1
+                    ? Conv2D.NewDyn(filters[part], kernel_size: kernelSize, padding: "same")
+                    : Conv2D.NewDyn(filters[part], kernel_size: (1, 1))));
+                this.batchNorms.Add(this.Track(new BatchNormalization()));
+            }
+        }
+
+        public override dynamic call(IEnumerable<IGraphNodeBase> inputs, ImplicitContainer<IGraphNodeBase> training, IGraphNodeBase mask) {
+            return this.callImpl((Tensor)inputs.Single(), training);
+        }
+
+        public override object call(object inputs, bool training, IGraphNodeBase mask = null) {
+            return this.callImpl((Tensor)inputs, training);
+        }
+
+        public override dynamic call(object inputs, ImplicitContainer<IGraphNodeBase> training = null, IEnumerable<IGraphNodeBase> mask = null) {
+            return this.callImpl((Tensor)inputs, training?.Value);
+        }
+
+        object callImpl(IGraphNodeBase inputs, dynamic training) {
+            IGraphNodeBase result = inputs;
+
+            var batchNormExtraArgs = new PythonDict<string, object>();
+            if (training != null)
+                batchNormExtraArgs["training"] = training;
+
+            for (int part = 0; part < PartCount; part++) {
+                result = this.convs[part].apply(result);
+                result = this.batchNorms[part].apply(result, kwargs: batchNormExtraArgs);
+                if (part + 1 != PartCount)
+                    result = tf.nn.relu(result);
+            }
+
+            result += (Tensor)result + inputs;
+
+            return tf.nn.relu(result);
+        }
+    }
+}
---
+++ ResNetBlock/ResNetSampleProgram.cs
@@ -0,0 +1,52 @@
+﻿namespace Gradient.Samples {
+    using System;
+    using System.Diagnostics;
+    using numpy;
+    using tensorflow;
+    using tensorflow.keras;
+    using tensorflow.keras.layers;
+    using tensorflow.train;
+
+    static class ResNetSampleProgram {
+        public static void Run(int epochs = 5) {
+            // requires Internet connection
+            (dynamic train, dynamic test) = tf.keras.datasets.fashion_mnist.load_data();
+            // will be able to do (trainImages, trainLabels) = train;
+            ndarray trainImages = np.expand_dims(train.Item1 / 255.0f, axis: 3);
+            ndarray trainLabels = train.Item2;
+            ndarray testImages = np.expand_dims(test.Item1 / 255.0f, axis: 3);
+            ndarray testLabels = test.Item2;
+
+            bool loaded = 60000 == trainImages.Length;
+            Debug.Assert(loaded);
+
+            var model = new Sequential(new Layer[] {
+                // will be able to do: new Flatten(kwargs: new { input_shape = (28, 28) }),
+                new ResNetBlock(kernelSize: 3, filters: new [] { 1, 2, 3 }),
+                new ResNetBlock(kernelSize: 3, filters: new [] { 1, 2, 3 }),
+                new Flatten(),
+                new Dense(units: 10, activation: tf.nn.softmax_fn),
+            });
+
+            model.compile(
+                optimizer: new AdamOptimizer(),
+                loss: "sparse_categorical_crossentropy",
+                metrics: new dynamic[] { "accuracy" });
+
+            model.fit(trainImages, trainLabels, epochs: epochs);
+
+            var testEvalResult = model.evaluate(testImages, testLabels);
+            double testAcc = testEvalResult[1];
+
+            Console.WriteLine($"Test accuracy: {testAcc}");
+            model.summary();
+        }
+
+        static void Main() {
+            Console.Title = nameof(ResNetSampleProgram);
+            GradientLog.OutputWriter = Console.Out;
+            GradientSetup.UseEnvironmentFromVariable();
+            Run();
+        }
+    }
+}
