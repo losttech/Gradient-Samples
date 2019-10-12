5c2985dd84c7ae7315042e888a4cddc135a9ebef
Victor <lost@losttech.software>
2019-10-11 18:02:00 -0700

    add C# or NOT sample (#3)

---
+++ CSharpOrNot/CSharpOrNot.cs
@@ -0,0 +1,115 @@
+﻿namespace Gradient.Samples {
+    using System;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using numpy;
+    using tensorflow;
+    using tensorflow.keras;
+    using tensorflow.keras.layers;
+
+    static class CSharpOrNot
+    {
+        public static Model CreateModel(int classCount) {
+            var activation = tf.keras.activations.elu_fn;
+            const int filterCount = 8;
+            int[] resNetFilters = { filterCount, filterCount, filterCount };
+            return new Sequential(new Layer[] {
+                new Dropout(rate: 0.05),
+                Conv2D.NewDyn(filters: filterCount, kernel_size: 5, padding: "same"),
+                Activation.NewDyn(activation),
+                new MaxPool2D(pool_size: 2),
+                new ResNetBlock(kernelSize: 3, filters: resNetFilters, activation: activation),
+                new ResNetBlock(kernelSize: 3, filters: resNetFilters, activation: activation),
+                new ResNetBlock(kernelSize: 3, filters: resNetFilters, activation: activation),
+                new ResNetBlock(kernelSize: 3, filters: resNetFilters, activation: activation),
+                new MaxPool2D(),
+                new ResNetBlock(kernelSize: 3, filters: resNetFilters, activation: activation),
+                new ResNetBlock(kernelSize: 3, filters: resNetFilters, activation: activation),
+                new MaxPool2D(),
+                new ResNetBlock(kernelSize: 3, filters: resNetFilters, activation: activation),
+                new ResNetBlock(kernelSize: 3, filters: resNetFilters, activation: activation),
+                new AvgPool2D(pool_size: 2),
+                new Flatten(),
+                new Dense(units: classCount, activation: tf.nn.softmax_fn),
+            });
+        }
+
+        public const int Width = 64, Height = 64;
+        public static readonly Size Size = new Size(Width, Height);
+        // being opinionated here
+        const string Tab = "    ";
+        const char Whitespace = '\u00FF';
+        public static readonly string[] IncludeExtensions = {
+            ".cs",
+            ".py",
+            ".h",
+            ".cc",
+            ".c",
+            ".tcl",
+            ".java",
+            ".sh",
+        };
+
+        public static ndarray<float> GreyscaleImageBytesToNumPy(byte[] inputs, int imageCount, int width, int height)
+            => (dynamic)inputs.Select(b => (float)b).ToArray().NumPyCopy()
+                .reshape(new[] { imageCount, height, width, 1 }) / 255.0f;
+
+        public static string[] ReadCode(string filePath)
+            => File.ReadAllLines(filePath)
+                .Select(line => line.Replace("\t", Tab))
+                .Select(line => {
+                    var result = new StringBuilder(line.Length);
+                    // replace non-ASCII characters with underscore
+                    // also make all whitespace stand out
+                    foreach (char c in line) {
+                        result.Append(
+                            c <= 32 ? Whitespace
+                            : c >= 255 ? '_'
+                            : c);
+                    }
+                    return result.ToString();
+                })
+                .ToArray();
+
+        /// <summary>
+        /// Copies a rectangular block of text into a byte array
+        /// </summary>
+        public static void RenderTextBlockToGreyscaleBytes(string[] lines,
+            Point startingPoint, Size size,
+            byte[] destination)
+        {
+            if (size.IsEmpty) throw new ArgumentException();
+            if (destination.Length < size.Width * size.Height) throw new ArgumentException();
+            if (startingPoint.Y == lines.Length) {
+                Array.Fill(destination, (byte)Whitespace);
+                return;
+            }
+
+            for (int y = 0; y < size.Height; y++) {
+                int sourceY = y + startingPoint.Y;
+                int destOffset = y * size.Width;
+                if (sourceY >= lines.Length) {
+                    Array.Fill(destination, (byte)255,
+                        startIndex: destOffset,
+                        count: size.Width*size.Height - destOffset);
+                    return;
+                }
+
+                for (int x = 0; x < size.Width; x++) {
+                    int sourceX = x + startingPoint.X;
+                    if (sourceX >= lines[sourceY].Length) {
+                        Array.Fill(destination, (byte)255,
+                            startIndex: destOffset,
+                            count: size.Width - x);
+                        break;
+                    }
+
+                    destination[destOffset] = (byte)lines[sourceY][sourceX];
+                    destOffset++;
+                }
+            }
+        }
+    }
+}
--- ResNetBlock/ResNetBlock.cs
+++ ResNetBlock/ResNetBlock.cs
@@ -7,17 +7,22 @@
     using tensorflow.keras;
     using tensorflow.keras.layers;
 
-    class ResNetBlock: Model {
+    public class ResNetBlock: Model {
         const int PartCount = 3;
         readonly PythonList<Conv2D> convs = new PythonList<Conv2D>();
         readonly PythonList<BatchNormalization> batchNorms = new PythonList<BatchNormalization>();
-        public ResNetBlock(int kernelSize, int[] filters) {
+        readonly PythonFunctionContainer activation;
+        readonly int outputChannels;
+        public ResNetBlock(int kernelSize, int[] filters, PythonFunctionContainer activation = null) {
+            this.activation = activation ?? tf.keras.activations.relu_fn;
             for (int part = 0; part < PartCount; part++) {
                 this.convs.Add(this.Track(part == 1
-                    ? Conv2D.NewDyn(filters[part], kernel_size: kernelSize, padding: "same")
+                    ? Conv2D.NewDyn(filters: filters[part], kernel_size: kernelSize, padding: "same")
                     : Conv2D.NewDyn(filters[part], kernel_size: (1, 1))));
                 this.batchNorms.Add(this.Track(new BatchNormalization()));
             }
+
+            this.outputChannels = filters[PartCount - 1];
         }
 
         public override dynamic call(IEnumerable<IGraphNodeBase> inputs, ImplicitContainer<IGraphNodeBase> training, IGraphNodeBase mask) {
@@ -43,12 +48,22 @@
                 result = this.convs[part].apply(result);
                 result = this.batchNorms[part].apply(result, kwargs: batchNormExtraArgs);
                 if (part + 1 != PartCount)
-                    result = tf.nn.relu(result);
+                    result = ((dynamic)this.activation)(result);
             }
 
-            result += (Tensor)result + inputs;
+            result = (Tensor)result + inputs;
 
-            return tf.nn.relu(result);
+            return ((dynamic)this.activation)(result);
+        }
+
+        public override dynamic compute_output_shape(TensorShape input_shape) {
+            if (input_shape.ndims == 4) {
+                var outputShape = input_shape.as_list();
+                outputShape[3] = this.outputChannels;
+                return new TensorShape(outputShape);
+            }
+
+            return input_shape;
         }
     }
 }
