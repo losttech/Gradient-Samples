53802cc97d67f22230879be510e784292f47724a
Victor Milovanov <lost@losttech.software>
2020-02-07 14:10:51 -0800

    soft actor critic RL sample with Unity ML agents and 3D Ball environment

---
+++ RL-MLAgents/IEnvironment.cs
@@ -0,0 +1,29 @@
+﻿namespace Gradient.Samples {
+    using System;
+    using System.Collections.Generic;
+    using mlagents_envs.base_env;
+    using numpy;
+
+    /// <summary>
+    /// A simple environment interface
+    /// </summary>
+    interface IEnvironment {
+        /// <summary>
+        /// Reset the environment to its initial stage
+        /// </summary>
+        void Reset();
+        /// <summary>
+        /// Advance time by 1 tick/step in the environment
+        /// </summary>
+        void Step();
+        /// <summary>
+        /// Get information about the last tick/step in the environment, as seen by a group of agents
+        /// </summary>
+        BatchedStepResult GetStepResult(string? agentGroupName);
+        /// <summary>
+        /// Call before <see cref="Step"/> to set the action(s), that agent(s) in the specified
+        /// group will do during the next time step
+        /// </summary>
+        void SetActions(string? agentGroupName, ndarray actions);
+    }
+}
---
+++ RL-MLAgents/SoftActorCritic/Policy.cs
@@ -0,0 +1,16 @@
+﻿namespace Gradient.Samples.SoftActorCritic {
+    using System;
+    using tensorflow;
+
+    struct Policy {
+        public Tensor mu;
+        public Tensor pi;
+        public readonly Tensor logProbPi;
+
+        public Policy(Tensor mu, Tensor pi, Tensor logProbPi) {
+            this.mu = mu ?? throw new ArgumentNullException(nameof(mu));
+            this.pi = pi ?? throw new ArgumentNullException(nameof(pi));
+            this.logProbPi = logProbPi ?? throw new ArgumentNullException(nameof(logProbPi));
+        }
+    }
+}
